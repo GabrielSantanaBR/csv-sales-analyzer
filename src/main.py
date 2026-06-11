4ea6ee94f4cfac447b2ea6ffef4b652306f0e271
Gabriel <gabrielbsantana61@gmail.com>
2026-06-11 20:37:35 -0300

    feat: add command line interface with argparse

--- src/main.py
+++ src/main.py
@@ -2,12 +2,30 @@ from analyzer import load_data, calculate_metrics
 from report import print_report, export_report
 from charts import create_charts
 
-df = load_data("sales.csv")
+import argparse
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--report", action="store_true")
+parser.add_argument("--export", action="store_true")
+parser.add_argument("--charts", action="store_true")
+parser.add_argument("--all", action="store_true")
+
+args = parser.parse_args()
+
+df = load_data("src/sales.csv")
 metrics = calculate_metrics(df)
 
-print_report(metrics)
+if args.report:
+    print_report(metrics)
 
-export_report(df)
+if args.export:
+    export_report(df)
 
-create_charts(df)
+if args.charts:
+    create_charts(df)
+
+if args.all:
+    print_report(metrics)
+    export_report(df)
+    create_charts(df)
